53fb6e7f9f47f05ec5c9c29470824194fce293a7
Nadin <icemylady@gmail.com>
2022-11-25 22:00:02 +0300

    Решение заыач 64,66

---
+++ Zadacha64/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в 
+//промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5-> "5, 4, 3, 2, 1"
+// N = 8-> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+string NumbersRec(int N, int b)
+{
+    if (N >= b) return $"{N} " + NumbersRec(N - 1, b);
+    else return string.Empty;
+}
+Console.Write("Задайте значение N ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(NumbersRec(N, 1));
---
+++ Zadacha66/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов
+// в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+Console.WriteLine("Задайте значение M ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте значение N ");
+int n = Convert.ToInt32(Console.ReadLine());
+int SummRecursion(int n)
+{
+    if (m > n) return 0;
+    else return n + SummRecursion(n - 1);
+}
+
+Console.WriteLine(SummRecursion(n));
+
